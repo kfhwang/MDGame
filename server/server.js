1a3219a4759f6a92b676ce5111865051222ec9b6
unknown <kfhwang@gm.nutc.edu.tw>
2022-05-03 15:06:58 +0800

    20220503

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var server = express();
+ 
 
 server.use(express.static(__dirname+'/public'));
-
-
+ 
 server.get("/", function(req, res){
     res.send("Hello, world!");
 })
