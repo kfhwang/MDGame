fd2889c3e21e608b93846e79b2f44d7b1df2191d
kfhwang <kfhwang@gm.nutc.edu.tw>
2022-05-10 15:22:30 +0800

    20220510

---
+++ server/rest.js
@@ -0,0 +1,10 @@
+
+
+
+POST https://localhost/message HTTP/1.1
+content-type: application/json
+
+{
+    "fname": "Jonh1",
+    "lname": "Happer1"
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var server = express();
  
+var bodyParser = require("body-parser");
+server.use(bodyParser.urlencoded({limit:"2mb"}));
+server.use(bodyParser.json({limit:"2mb"}));
 
 server.use(express.static(__dirname+'/public'));
  
@@ -13,11 +16,16 @@ server.get("/message", function(req, res){
     res.send("Hello "+ req.query.fname+" "+req.query.lname);
 })
 
+server.post("/message", function(req, res){
+    console.log(req.body);
+    res.send("Hello "+ req.body.fname+" "+req.body.lname);
+});
+
 
 server.get("*", function(req, res){
     res.send("Page not found", 404);
 })
 
-server.listen(8080);
+server.listen(80);
 
 
