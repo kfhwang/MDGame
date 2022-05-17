a7f73af4f6fcb7012e2d52e06553483a97e0de2d
kfhwang <kfhwang@gm.nutc.edu.tw>
2022-05-17 15:53:06 +0800

    20220517

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ var https = require("https");
 var pkey = fs.readFileSync(__dirname+'/privkey.pem');
 var hcert = fs.readFileSync(__dirname+'/fullchain.pem');
 
+//NEDB
+const DB = require('nedb-promises')
+let GameDB = DB.create(__dirname+'/game.db');
 
 var bodyParser = require("body-parser");
 server.use(bodyParser.urlencoded({limit:"2mb"}));
@@ -27,8 +30,24 @@ server.post("/message", function(req, res){
     console.log(req.body);
     res.send("Hello "+ req.body.fname+" "+req.body.lname);
 });
-
-
+//接收gdevelop 傳來的資料
+server.post("/game", function(req, res){
+    console.log(req.body);
+    //save to DB
+    GameDB.insert(req.body).then(function(result){
+        res.json({"status":"OK"});
+    })
+    
+})
+//回傳目前的遊戲紀錄
+server.post("/record", function(req, res){
+    if(req.body.score==0){
+        res.json({"level":2,"score":2000, "user":"MD"});
+    }else{
+       res.json({"score":0, "repeat":"Error"})
+    }
+    
+})
 server.get("*", function(req, res){
     res.send("Page not found", 404);
 })
